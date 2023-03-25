588f341d0a6742fbadf12c2c3edd2f636616d19a
CapitanHomer <118437119+CapitanHomer@users.noreply.github.com>
2023-03-25 19:01:04 +0100

    post_registre

---
+++ module/login/ctrl/ctrl_login.php
@@ -0,0 +1,163 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'] . '/MVC_cars_V10/';
+include($path . "/module/login/model/DAO_login.php");
+session_start();
+
+switch ($_GET['op']) {
+    
+    case 'login-register_view';
+   
+        include("module/login/view/login_register.html");
+        break;
+
+    case 'register':
+        // echo json_encode("hola case register");
+        // exit;
+    //     // Comprobar que la email no exista
+    // echo json_encode($_POST['email_reg']);
+    //         exit;
+        try {
+            $daoLog = new DAOLogin();
+            $check = $daoLog->control_registro_mail($_POST['email_reg']);
+        } catch (Exception $e) {
+            echo json_encode("error");
+            exit;
+        }
+        try {
+            $daoLog = new DAOLogin();
+            $check1 = $daoLog->control_registro_user($_POST['username_reg']);
+        } catch (Exception $e) {
+            echo json_encode("error");
+            exit;
+        }
+        // echo json_encode($check);
+        // exit;
+        if ($check || $check1) {
+            $check_control = false;
+        } else {
+            // echo json_encode("error_email");
+            $check_control = true;
+        }
+
+        // Si no existe el email creará el usuario
+        if ($check_control == true) {
+            try {
+                $daoLog = new DAOLogin();
+                $rdo = $daoLog->insert_user($_POST['username_reg'], $_POST['email_reg'], $_POST['passwd1_reg']);
+            } catch (Exception $e) {
+                echo json_encode("error");
+                exit;
+            }
+            if (!$rdo) {
+                echo json_encode("error_user");
+                exit;
+            } else {
+                echo json_encode("ok");
+                exit;
+            }
+        } 
+        // else {
+        //     echo json_encode("error_email");
+        //     exit;
+        // }
+        if($check){
+            echo json_encode("error_email");
+            exit;
+        }else if($check1){
+            echo json_encode("error_user");
+            exit;
+        }
+        break;
+    
+    case 'login':
+        // echo json_encode("hola case login");
+        // exit;
+        try {
+            $daoLog = new DAOLogin();
+            $rdo = $daoLog->seleccionar_usuario($_POST['username_log']);
+
+            if ($rdo == "error_user") {
+                echo json_encode("error_user");
+                exit;
+            } else {
+                if (password_verify($_POST['passwd_log'], $rdo['password'])) {
+                    // $token= create_token($rdo["username"]);
+                    $_SESSION['username'] = $rdo['username']; //Guardamos el usario 
+                    $_SESSION['tiempo'] = time(); //Guardamos el tiempo que se logea
+                    echo json_encode($token);
+                    exit;
+                } else {
+                    echo json_encode("error_passwd");
+                    exit;
+                }
+            }
+        } catch (Exception $e) {
+            echo json_encode("error");
+            exit;
+        }
+        break;
+
+    // case 'logout':
+    //     unset($_SESSION['username']);
+    //     unset($_SESSION['tiempo']);
+    //     session_destroy();
+
+    //     echo json_encode('Done');
+    //     break;
+
+    // case 'data_user':
+    //     $json = decode_token($_POST['token']);
+    //     $daoLog = new DAOLogin();
+    //     $rdo = $daoLog->select_data_user($json['username']);
+    //     echo json_encode($rdo);
+    //     exit;
+    //     break;
+
+    // case 'actividad':
+    //     if (!isset($_SESSION["tiempo"])) {
+    //         echo json_encode("inactivo");
+    //         exit();
+    //     } else {
+    //         if ((time() - $_SESSION["tiempo"]) >= 1800) { //1800s=30min
+    //             echo json_encode("inactivo");
+    //             exit();
+    //         } else {
+    //             echo json_encode("activo");
+    //             exit();
+    //         }
+    //     }
+    //     break;
+
+    // case 'controluser':
+    //     $token_dec = decode_token($_POST['token']);
+
+    //     if ($token_dec['exp'] < time()) {
+    //         echo json_encode("Wrong_User");
+    //         exit();
+    //     }
+
+    //     if (isset($_SESSION['username']) && ($_SESSION['username']) == $token_dec['username']) {
+    //         echo json_encode("Correct_User");
+    //         exit();
+    //     } else {
+    //         echo json_encode("Wrong_User");
+    //         exit();
+    //     }
+    //     break;
+
+    // case 'refresh_token':
+    //     $old_token = decode_token($_POST['token']);
+    //     $new_token = create_token($old_token['username']);
+    //     echo json_encode($new_token);
+    //     break;
+
+    // case 'refresh_cookie':
+    //     session_regenerate_id();
+    //     echo json_encode("Done");
+    //     exit;
+    //     break;
+
+    default;
+        include("module/views/inc/error404.php");
+        break;
+}
---
+++ module/login/model/ctrl_login.js
@@ -0,0 +1,210 @@
+///////////////////////REGISTER/////////////////////////////////////////////////////////////////
+function register() {
+    console.log("hola register");
+    if (validate_register() != 0) {
+        var data = $('#register__form').serialize();
+        console.log(data);
+        ajaxPromise('module/login/ctrl/ctrl_login.php?op=register', 'POST', 'JSON', data)
+            .then(function(result) {
+                console.log(result);
+                if (result == "error_email") {
+                    console.log("hola1");
+                    document.getElementById('error_email_reg').innerHTML = "El email ya esta en uso, asegurate de no tener ya una cuenta"
+                } else if (result == "error_user") {
+                    console.log("hola2");
+                    document.getElementById('error_username_reg').innerHTML = "El usuario ya esta en uso, intentalo con otro"
+                } else {
+                    console.log("hola3");
+                    toastr.success("Registery succesfully");
+                    // setTimeout(' window.location.href = "index.php?module=ctrl_login&op=login-register_view"; ', 1000);
+                    // $(document).style('background: green');
+                    // window.location.reload();
+                    $('#jump').click();
+                    $('#username_reg').val("");
+                    $('#email_reg').val("");
+                    $('#passwd1_reg').val("");
+                    $('#passwd2_reg').val("");
+                }
+            }).catch(function(textStatus) {
+                // if (console && console.log) {
+                    console.log("La solicitud ha fallado: " + textStatus);
+                // }
+            });
+    }
+}
+
+function key_register() {
+    $("#register").keypress(function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) {
+            e.preventDefault();
+            register();
+        }
+    });
+}
+
+function button_register() {
+    $('#register').on('click', function(e) {
+        e.preventDefault();
+        register();
+    });
+}
+
+function validate_register() {
+    console.log("hola validate register");
+    var username_exp = /^[a-zA-Z0-9](?!.*__)[a-zA-Z0-9_]{4,18}[a-zA-Z0-9]$/;
+    var mail_exp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    var pssswd_exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*])[A-Za-z\d!@#$%&*]{8,20}$/;
+    var error = false;
+
+    if (document.getElementById('username_reg').value.length === 0) {
+        document.getElementById('error_username_reg').innerHTML = "Tienes que escribir el usuario";
+        error = true;
+    } else {
+        if (document.getElementById('username_reg').value.length < 5) {
+            document.getElementById('error_username_reg').innerHTML = "El username tiene que tener 5 caracteres como minimo";
+            error = true;
+        } else {
+            if (!username_exp.test(document.getElementById('username_reg').value)) {
+                document.getElementById('error_username_reg').innerHTML = "No se pueden poner caracteres especiales";
+                error = true;
+            } else {
+                document.getElementById('error_username_reg').innerHTML = "";
+            }
+        }
+    }
+
+    if (document.getElementById('email_reg').value.length === 0) {
+        document.getElementById('error_email_reg').innerHTML = "Tienes que escribir un correo";
+        error = true;
+    } else {
+        if (!mail_exp.test(document.getElementById('email_reg').value)) {
+            document.getElementById('error_email_reg').innerHTML = "El formato del mail es invalido";
+            error = true;
+        } else {
+            document.getElementById('error_email_reg').innerHTML = "";
+        }
+    }
+
+    if (document.getElementById('passwd1_reg').value.length === 0) {
+        document.getElementById('error_passwd1_reg').innerHTML = "Tienes que escribir la contraseña";
+        error = true;
+    } else {
+        if (document.getElementById('passwd1_reg').value.length < 8) {
+            document.getElementById('error_passwd1_reg').innerHTML = "La password tiene que tener 8 caracteres como minimo";
+            error = true;
+        } else {
+            if (!pssswd_exp.test(document.getElementById('passwd1_reg').value)) {
+                document.getElementById('error_passwd1_reg').innerHTML = "Debe de contener minimo 8 caracteres, mayusculas, minusculas y simbolos especiales";
+                error = true;
+            } else {
+                document.getElementById('error_passwd1_reg').innerHTML = "";
+            }
+        }
+    }
+
+    if (document.getElementById('passwd2_reg').value.length === 0) {
+        document.getElementById('error_passwd2_reg').innerHTML = "Tienes que repetir la contraseña";
+        error = true;
+    } else {
+        if (document.getElementById('passwd2_reg').value.length < 8) {
+            document.getElementById('error_passwd2_reg').innerHTML = "La password tiene que tener 8 caracteres como minimo";
+            error = true;
+        } else {
+            if (document.getElementById('passwd2_reg').value === document.getElementById('passwd1_reg').value) {
+                document.getElementById('error_passwd2_reg').innerHTML = "";
+            } else {
+                document.getElementById('error_passwd2_reg').innerHTML = "La password's no coinciden";
+                error = true;
+            }
+        }
+    }
+
+    if (error == true) {
+        return 0;
+    }
+}
+//////////////////////////LOGIN//////////////////////////////////////////////////////////////7
+function login() {
+    console.log("hola login");
+    if (validate_login() != 0) {
+        console.log(validate_login);
+        var data = $('#login__form').serialize();
+        ajaxPromise('module/login/ctrl/ctrl_login.php?op=login', 'POST', 'JSON', data)
+            .then(function(result) {
+                console.log(data);
+                if (result == "error_user") {
+                    document.getElementById('error_username_log').innerHTML = "El usario no existe,asegurase de que lo a escrito correctamente"
+                } else if (result == "error_passwd") {
+                    document.getElementById('error_passwd_log').innerHTML = "La contraseña es incorrecta"
+                } else {
+                    localStorage.setItem("token", result);
+                    toastr.success("Loged succesfully");
+
+                    if (localStorage.getItem('redirect_like')) {
+                        setTimeout(' window.location.href = "index.php?module=ctrl_shop&op=list"; ', 1000);
+                    } else {
+                        setTimeout(' window.location.href = "index.php?module=ctrl_home&op=list"; ', 1000);
+                    }
+                }
+            }).catch(function(textStatus) {
+                if (console && console.log) {
+                    console.log("La solicitud ha fallado: " + textStatus);
+                }
+            });
+    }
+}
+
+function key_login() {
+    $("#login").keypress(function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) {
+            e.preventDefault();
+            login();
+        }
+    });
+}
+
+function button_login() {
+    console.log("hola button");
+    $('#login').on('click', function(e) {
+        e.preventDefault();
+        login();
+    });
+}
+
+function validate_login() {
+    console.log("hola validate_login");
+    var error = false;
+
+    if (document.getElementById('username_log').value.length === 0) {
+        document.getElementById('error_username_log').innerHTML = "Tienes que escribir el usuario";
+        error = true;
+    } else {
+        if (document.getElementById('username_log').value.length < 5) {
+            document.getElementById('error_username_log').innerHTML = "El usuario tiene que tener 5 caracteres como minimo";
+            error = true;
+        } else {
+            document.getElementById('error_username_log').innerHTML = "";
+        }
+    }
+
+    if (document.getElementById('passwd_log').value.length === 0) {
+        document.getElementById('error_passwd_log').innerHTML = "Tienes que escribir la contraseña";
+        error = true;
+    } else {
+        document.getElementById('error_passwd_log').innerHTML = "";
+    }
+
+    if (error == true) {
+        return 0;
+    }
+}
+
+$(document).ready(function() {
+// console.log("hola ready");
+    key_register();
+    button_register();
+    key_login();
+    button_login();
+});
